bb2c33d0ec5369fbc1bc22f9025f90515f513ef8
Pablo <araujopablo95@gmail.com>
2026-08-18 17:04:32 -0300

    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -25,6 +25,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
     },
     {
+      url: `${BASE_URL}/rider`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.6,
+    },
+    {
       url: `${BASE_URL}/contacto`,
       lastModified: new Date(),
       changeFrequency: "monthly",
